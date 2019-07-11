abee732e58d599acbe4278201fb8e377e6a6954e
Slav666 <slawomirdyk@gmail.com>
2019-07-11 20:11:16 +0100

    Delete button works! React is not stupid, I am stupid

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { getMovies } from '../services/fakeMovieService';
+// import { smart } from '@babel/template';
 
 
 class Movies extends Component {
@@ -8,7 +9,10 @@ class Movies extends Component {
         movies: getMovies()
     };
 
-    
+    moviesDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies});
+    };
 
     
     render() { 
@@ -23,15 +27,17 @@ class Movies extends Component {
             </thead>
             <tbody>
                 {this.state.movies.map(movie => (
-                <tr>
+                <tr key={movie._id}>
                     <td>{movie.title}</td>
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate}</td>
+                    <td><button onClick={() => this.moviesDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                 </tr>
                 ))}
             </tbody>
         </table>
+        
     }
 }
  
